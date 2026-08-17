1a75e827660e940e8b4a218b62f78665f2cced55
AitorJury <aitor.jr04@gmail.com>
2026-08-17 17:15:51 +0200

    Finish Project 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   image: {
-    remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
+    remotePatterns: [{ protocol: 'https', hostname: '**.picsum.photos' }],
   },
 });
--- src/content/productos/prueba.md
+++
@@ -1,7 +0,0 @@
----
-title: Prueba
-image: https://picsum.photos/id/360/1920/1280.webp
-class: ''
----
-
-Esto es una prueba.
